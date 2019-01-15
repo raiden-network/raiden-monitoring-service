2b6fe469dbf0831648b52b82f69de18d9ba4aff6
Karl Bartel <karl42@gmail.com>
2019-01-15 17:18:25 +0100

    Remove chunk callbacks

--- monitoring_service/utils/blockchain_listener.py
+++ monitoring_service/utils/blockchain_listener.py
@@ -146,7 +146,6 @@ class BlockchainListener(gevent.Greenlet):
 
         self.confirmed_callbacks: Dict[int, Tuple[List, Callable]] = {}
         self.unconfirmed_callbacks: Dict[int, Tuple[List, Callable]] = {}
-        self.chunk_callbacks: List[Callable] = []
         self.state_handler = state_handler
 
         self.wait_sync_event = gevent.event.Event()
@@ -178,14 +177,6 @@ class BlockchainListener(gevent.Greenlet):
         """ Add a callback to listen for unconfirmed events. """
         self.unconfirmed_callbacks[self.counter] = (topics, callback)
         self.counter += 1
-
-    def add_chunk_callback(self, callback: Callable):
-        """ Trigger callback after processing a chunk of blocks.
-
-        This is useful to save the processing state to disk. The callback gets
-        called the BlockchainListener as argument.
-        """
-        self.chunk_callbacks.append(callback)
 
     def _run(self):
         self.running = True
